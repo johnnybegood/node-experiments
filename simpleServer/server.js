b28c5028fa83e906894933f6fd067b5ee9ffc99e
Wouter Hemeryck <wouter@johnnybegood.be>
2013-07-21 13:18:36 +0200

    Empty template

--- simpleServer/server.js
+++ simpleServer/server.js
@@ -20,7 +20,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
